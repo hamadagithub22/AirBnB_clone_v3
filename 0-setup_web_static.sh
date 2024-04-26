9e38ba2478f4504a3d9d98a84a9110ae94643686
hamadastore1122 <shehabstore22@gmail.com.com>
2024-04-26 16:33:43 -0700

    AirBnB_clone

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#sets up ther  web servers forthe deployment of web_static
+# sets up the web servers for the deployment of web_static
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install ngix
-sudo mkdiir -p /data/web_static/releases/relreases/test /data/web_static/shared
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
